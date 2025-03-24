25df90b7277fa8ac6907b4cab111ecf848135af8
Luke Searle <luke.searle@eeca.govt.nz>
2025-03-24 15:38:57 +1300

    added full current pipeline to main prep script

--- PREPARE-TIMES-NZ/scripts/prepare_times_nz.py
+++ PREPARE-TIMES-NZ/scripts/prepare_times_nz.py
@@ -14,6 +14,12 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "..", "library"))
 from filepaths import PREP_LOCATION, DATA_INTERMEDIATE, OUTPUT_LOCATION
 
+STAGE_0_SCRIPTS = f"{PREP_LOCATION}/scripts/stage_0_settings/"
+STAGE_1_SCRIPTS = f"{PREP_LOCATION}/scripts/stage_1_prep_raw_data/"
+STAGE_2_SCRIPTS = f"{PREP_LOCATION}/scripts/stage_2_baseyear/"
+# STAGE 3 SCRIPTS dont exist yet 
+STAGE_4_SCRIPTS = f"{PREP_LOCATION}/scripts/stage_4_veda_format/"
+
 # NOTE: here `INPUT_LOCATION` refers to the intermediate files that will be created in data_intermediate
 
 # file locations 
@@ -38,22 +44,37 @@ os.makedirs(DATA_INTERMEDIATE)
 method = "times_3" 
 
 
-# Execute 
+# Execute TIMES 2
 
 if method == "times_2":
     print(f"Reading the archived summary data")
     os.system(f"python {PREP_LOCATION}/scripts/times_2_methods/read_archive_summary.py")
     print(f"Creating TIMES excel files in {OUTPUT_LOCATION}")
-    os.system(f"python {PREP_LOCATION}/scripts/times_2_methods/prepare_times_nz_from_archive.py")
-    
-    
-# 
+    os.system(f"python {PREP_LOCATION}/scripts/times_2_methods/prepare_times_nz_from_archive.py")       
+
+# Execute TIMES 3
 
 if method == "times_3":
-    os.system(f"python {PREP_LOCATION}/scripts/stage_0_settings/parse_tomls.py")    
 
+    # Stage 0: Settings 
+    print(f"Reading settings files...")    
+    os.system(f"python {STAGE_0_SCRIPTS}/parse_tomls.py")    
+
+    # Stage 1: Prep raw data 
+    print(f"Preparing raw data...")    
+    os.system(f"python {STAGE_1_SCRIPTS}/extract_ea_data.py")    
+    os.system(f"python {STAGE_1_SCRIPTS}/extract_mbie_data.py")    
+
+    # Stage 2: Base Year 
+    print(f"Compiling base year files...")    
+    os.system(f"python {STAGE_2_SCRIPTS}/baseyear_electricity_generation.py")    
+
+    # Stage 3: Scenarios:
+    # no scripts exist yet. 
+
+    #Stage 4: Create excel files 
     print(f"Building TIMES excel files based on .toml configuration files...")    
-    os.system(f"python {PREP_LOCATION}/scripts/stage_4_veda_format/write_excel.py")    
+    os.system(f"python {STAGE_4_SCRIPTS}/write_excel.py")    
     print(f"Job complete")
     
 
