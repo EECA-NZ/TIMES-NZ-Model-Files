47d586d9fdafd12cface2cb4b64c58e859f38221
Luke Searle <luke.searle@eeca.govt.nz>
2025-10-31 09:46:53 +1300

    toml for closures and attempted bugfix for ele

--- PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_baseyear_elc_files.py
+++ PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_baseyear_elc_files.py
@@ -328,7 +328,8 @@ def define_generation_parameters(df):
 
     # additional hard-coded parameters
     # no new investment
-    existing_techs_parameters["NCAP_BND"] = 0
+    # existing_techs_parameters["NCAP_BND"] = 0
+    existing_techs_parameters[f"NCAP_BND~{BASE_YEAR+1}"] = 0
     existing_techs_parameters["NCAP_BND~0"] = 5
     # standard cap2act method (should this not go in FI_Process?)
     existing_techs_parameters["CAP2ACT"] = CAP2ACT_PJGW
