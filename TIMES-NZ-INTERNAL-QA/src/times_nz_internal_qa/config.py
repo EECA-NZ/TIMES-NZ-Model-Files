bd77c91b7289ca25373d501c34dc606024247c46
Luke Searle <luke.searle@eeca.govt.nz>
2025-11-07 10:16:08 +1300

    adjust scen list

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
@@ -4,7 +4,6 @@ Surfaces some config options for preprocessing and the app
 
 current_scenarios = [
     "traditional-v3_0_0",
-    "traditional-v3_0_0-af_res",
     "transformation-v3_0_0",
     # "traditional-v3_0_0-highcarbon",
 ]
