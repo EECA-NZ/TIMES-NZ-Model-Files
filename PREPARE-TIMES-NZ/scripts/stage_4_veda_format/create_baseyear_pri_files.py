a24e82ae833666f50a6df07d97f016b172382c85
Achini Weerasinghe <achini.weerasinghe@eeca.govt.nz>
2025-11-11 16:06:16 +1300

    biofuel supply forecast

--- PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_baseyear_pri_files.py
+++ PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_baseyear_pri_files.py
@@ -4,6 +4,8 @@ Module executes all the oil and gas veda transformations
 Currently just does the oil gas veda scripts
 """
 
+from prepare_times_nz.stage_4.biofuels import main as biofuels_veda
 from prepare_times_nz.stage_4.oil_and_gas import main as oil_gas_veda
 
 oil_gas_veda()
+biofuels_veda()
