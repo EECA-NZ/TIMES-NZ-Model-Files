0f8adb3ab5a3eb90d6dfd5e44b66bd5d04bf770f
Luke Searle <luke.searle@eeca.govt.nz>
2025-11-14 11:09:34 +1300

    more base year tech bans

--- PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_baseyear_demand_files.py
+++ PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_baseyear_demand_files.py
@@ -9,6 +9,7 @@ Runs all baseyear veda file processing for each demand sector:
 
 from prepare_times_nz.stage_4.baseyear.ag_forest_fish import main as ag
 from prepare_times_nz.stage_4.baseyear.commercial import main as com
+from prepare_times_nz.stage_4.baseyear.declare_banned_techs import main as ban
 from prepare_times_nz.stage_4.baseyear.industrial import main as ind
 from prepare_times_nz.stage_4.baseyear.residential import main as res
 from prepare_times_nz.stage_4.baseyear.transport import main as tra
@@ -23,6 +24,9 @@ def main():
     ind()
     res()
     tra()
+    # additionally, build banned base year tech file
+    # all banned techs go here
+    ban()
 
 
 if __name__ == "__main__":
