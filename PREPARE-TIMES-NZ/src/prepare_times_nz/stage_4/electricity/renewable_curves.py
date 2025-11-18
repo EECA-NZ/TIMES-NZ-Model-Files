44bd49082fdb9e40acca6f1e422644f078d6d3df
Luke Searle <luke.searle@eeca.govt.nz>
2025-11-18 16:16:17 +1300

    more robust baseload plant settings

--- PREPARE-TIMES-NZ/src/prepare_times_nz/stage_4/electricity/renewable_curves.py
+++ PREPARE-TIMES-NZ/src/prepare_times_nz/stage_4/electricity/renewable_curves.py
@@ -18,9 +18,17 @@ and we don't want to be updating this every time we switch a scenario run
 import numpy as np
 import pandas as pd
 from prepare_times_nz.utilities.data_in_out import _save_data
-from prepare_times_nz.utilities.filepaths import ASSUMPTIONS, STAGE_4_DATA
+from prepare_times_nz.utilities.filepaths import (
+    ASSUMPTIONS,
+    CONCORDANCES,
+    STAGE_2_DATA,
+    STAGE_4_DATA,
+)
 
 input_file = ASSUMPTIONS / "electricity_generation/renewable_curves/RenewableCurves.csv"
+cap_factors = ASSUMPTIONS / "electricity_generation/CapacityFactors.csv"
+
+tech_codes = CONCORDANCES / "electricity/tech_codes.csv"
 
 
 OUTPUT_LOCATION = STAGE_4_DATA / "scen_ren_af"
@@ -41,14 +49,14 @@ def generate_ren_af_file():
     # define all the techs with fixed af codes - other techs get "upper" (ie wind/hydro)
     # this is another way of saying we expect wind to spill first if we're in a spill situation
     # (or that solar/geo don't spill)
-    fixed_af_techs = ["Geo", "SolarDist", "SolarTrack", "SolarFixed"]
+    fixed_af_techs = ["SolarDist", "SolarTrack", "SolarFixed"]
 
     # additional requirements
     df["LimType"] = np.where(df["TechCode"].isin(fixed_af_techs), "FX", "UP")
     df["Attribute"] = "NCAP_AF"
 
     # generate wildcards
-    df["Pset_PN"] = "ELC_" + df["TechCode"] + "*"
+    df["Pset_PN"] = "ELC_" + df["TechCode"] + "_*"
 
     # trim to necessary columns
     df = df[["TimeSlice", "LimType", "Attribute", "NI", "SI", "Pset_PN"]]
@@ -56,9 +64,56 @@ def generate_ren_af_file():
     return df
 
 
+def generate_baseload_file():
+    """
+    Set AF for CHP plants as fixed across all slices
+    This means baseload, effectively
+    We'll do it for geo too
+
+
+    """
+
+    yrfr = pd.read_csv(STAGE_2_DATA / "settings/load_curves/yrfr.csv")
+
+    slices = yrfr[["TimeSlice"]]
+
+    cf = pd.read_csv(cap_factors)
+    tc = pd.read_csv(tech_codes)
+
+    codes = tc.merge(cf, on=["TechnologyCode", "FuelType"])
+
+    # only cogen capfactors
+    codes = codes[
+        (codes["TechnologyCode"] == "COG") | (codes["FuelType"] == "Geothermal")
+    ]
+    codes = codes[["Tech_TIMES", "CapacityFactor"]].drop_duplicates()
+
+    # expand to all timeslices
+
+    df = codes.merge(slices, how="cross")
+
+    # labelling
+    df["NI"] = df["CapacityFactor"]
+    df["SI"] = df["CapacityFactor"]
+    df["LimType"] = "FX"
+    df["Attribute"] = "NCAP_AF"
+
+    # pset generation
+    df["Pset_PN"] = "ELC_" + df["Tech_TIMES"] + "_*"
+
+    # select key vars
+    df = df[["TimeSlice", "LimType", "Attribute", "NI", "SI", "Pset_PN"]]
+
+    return df
+
+
 def main():
     """script wrapper"""
-    df = generate_ren_af_file()
+    df_ren = generate_ren_af_file()
+    df_baseload = generate_baseload_file()
+
+    df = pd.concat([df_ren, df_baseload])
+
     _save_data(
         df,
         "renewable_availability.csv",
