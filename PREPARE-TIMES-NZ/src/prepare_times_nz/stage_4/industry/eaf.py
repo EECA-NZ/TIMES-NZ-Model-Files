e4d5a2272aad75e6e6e541b938ff86137e32757b
Luke Searle <luke.searle@eeca.govt.nz>
2025-11-25 21:34:15 +1300

    tidy toml

--- PREPARE-TIMES-NZ/src/prepare_times_nz/stage_4/industry/eaf.py
+++ PREPARE-TIMES-NZ/src/prepare_times_nz/stage_4/industry/eaf.py
@@ -1,74 +1,158 @@
 """
+Builds EAF demand projections and the relevant steel cogen reductions
 
-Builds new processes and commodities for the EAF
+to represent NZSteel transitioning to an EAF recycler.
 
-Note that the input assumptions for this include
-processes that have demand decrease when the EAF is installed.
-
+Note: the EAF implementation also requires other inputs, which are not scripted here.
 
 These are:
+a) subres files built to identify the new processes and commodities
+b) demand reductions for pure steel processes, such as coal feedstock for reduction
 
-a) Iron and Steel "motive power" STEEL-MOTOR-ISTEEL
-b) Iron and Steel "iron and steel manufacturing) STEEL-FURNC-ISTEEL
-c) coal feedstock STEEL-FDSTK-FDSTK
-d) wee also halve the generation of the cogen plant
-
-We also invent a new process just called EAF with the commodity demand "recycled steel"
-
-This just goes into a subres
-
-So we need a subres to introduce STEEL-ELC-EAF which produces ET
-
-I THINK the easiest way to do this is have the EAF produce those commodities
-So we don't mess with COM_PROJ, but rather force the EAF to produce them and reduce the generation
-
-Then, if the EAF is cheaper, it should just retire the old demand.
-
-BUT We do actually need to reduce the feedstock commodity demand
-
-So since we're in the com_proj then might as well reduce the commodity demand!
-
-Set EAF to 1 efficiency - its a 30MW draw at 50% capacity
-
-
-
-
-START HERE
-
-Build several files:
-
-SUBRES:
-EAF (tech and commodity)
-NewTech (tech and commodity)
-
-MODIFY BASE YEAR FILES
-
-ELC_CoalCHP_GlenbrookSteel should be COA in and INDELC out
-
-SCENARIO:
-
-new scenario for demand (not in demand section, confusingly)
-
-EAF demand trad/trans (this should include ELC_CoalCHP_GlenbrookSteel demand reductions)
-Newtech demand (only one, for trans)
-
-DEMAND MODIFICATIONS
-
-reduce the indices for base year commodities:
-
-STEEL-MOTOR-ISTEEL
-STEEL-FURNC-ISTEEL
-STEEL-FDSTK-FDSTK
-
-by half with initial EAF, then to 0 in full recycling scenario (trad/trans)
-
-(reduce gas also?? I dont think so?)
-
-
+This module only creates demand projections for the new recycled steel commodity
+and fixes the cogeneration levels in place (cogen falls when facility transitions from coal).
 
 """
 
-eaf_install_dates = [2026, 2036]
+import numpy as np
+import pandas as pd
+from prepare_times_nz.stage_0.stage_0_settings import BASE_YEAR
+from prepare_times_nz.utilities.data_in_out import _save_data
+from prepare_times_nz.utilities.filepaths import STAGE_4_DATA
+
+# CONSTANTS
+
+EAF_INSTALL_1 = 2026  # first install date (confirmed)
+EAF_INSTALL_2 = 2036  # second install date (only for a specific scenario)
+EAF_GW = 0.03  # 30 MW install
+EAF_PJ = EAF_GW * 31.536  # standard GW/PJ
+COGEN_PJ = 1.9426176  # from data (would be better to read directly!)
+END_YEAR = 2050
+
+EAF_DEMAND_CODE = "STEEL_FURNC-RSTEEL"
+STEEL_COGEN_CODE = "ELC_CoalCHP_GlenbrookSteel"
+
+OUTPUT_LOCATION = STAGE_4_DATA / "scen_demand/eaf"
 
 
-# read in
+def create_eaf_demand():
+    """
+    Compiles input constants into series of demand
+    and activity projections for the EAF
+
+    NOTE: the steel commodities with reduced demand are handled separately
+
+    """
+    df = pd.DataFrame()
+
+    df["Year"] = range(BASE_YEAR, END_YEAR + 1)
+
+    # expand for scenarios
+    df_a = df.assign(Scenario="Traditional")
+    df_b = df.assign(Scenario="Transformation")
+
+    df = pd.concat([df_a, df_b], ignore_index=True)
+
+    # add eaf count
+    df["eaf_count"] = np.where(df["Year"] >= EAF_INSTALL_1, 1, 0)
+    df["eaf_count"] = np.where(
+        (df["Year"] >= EAF_INSTALL_2) & (df["Scenario"] == "Transformation"),
+        df["eaf_count"] + 1,
+        df["eaf_count"],
+    )
+
+    # use eaf count to assign key variables
+    # EAF demand:
+    df[EAF_DEMAND_CODE] = EAF_PJ * df["eaf_count"]
+    # halve the cogen output for each eaf installation
+    df[STEEL_COGEN_CODE] = COGEN_PJ * (1 - df["eaf_count"] / 2)
+
+    return df
+
+
+def create_eaf_file(df, scenario, category):
+    """
+    Pulls the scenario/measure from the created data
+    and creates a single veda output file to save
+    """
+
+    df = df[df["Scenario"] == scenario].copy()
+    df = df[["Year", category]]
+    df["Source"] = category
+
+    # define key veda inputs
+
+    attribute = ""
+    interp = ""
+    type_varname = ""
+
+    if category == EAF_DEMAND_CODE:
+        attribute = "Demand"
+        interp = "2"
+        type_varname = "Cset_CN"
+
+    elif category == STEEL_COGEN_CODE:
+        attribute = "ACT_BND~FX"
+        interp = "5"
+        type_varname = "Pset_PN"
+
+    else:
+        raise ValueError("please review the EAF Veda inputs!")
+
+    # apply and reshape
+
+    df["0"] = interp
+    df["TimeSlice"] = ""
+    df["Region"] = "NI"
+    df["Attribute"] = attribute
+    df[type_varname] = category
+
+    index_vars = ["TimeSlice", "Region", "Attribute", type_varname, "0"]
+
+    # pivot years
+    df = (
+        df.pivot(columns="Year", values=category, index=index_vars)
+        .reset_index()
+        .rename_axis(None, axis=1)
+    )
+
+    return df
+
+
+def write_eaf_file(df, scenario, category):
+    """
+    A wrapper to generate the required file and save it with some parameters for names/labels
+
+    """
+
+    if category == "eaf_demand":
+        code = EAF_DEMAND_CODE
+    elif category == "steel_cogen":
+        code = STEEL_COGEN_CODE
+    else:
+        raise ValueError("Unknown category input, please review")
+
+    # create data
+    out = create_eaf_file(df, scenario, code)
+
+    # generate name and label
+
+    filename = f"{category}_{scenario.lower()}.csv"
+    filelabel = f"Saving {scenario} {category}"
+
+    # save
+    _save_data(out, name=filename, label=filelabel, filepath=OUTPUT_LOCATION)
+
+
+def main():
+    """Entrypoint"""
+    df = create_eaf_demand()
+
+    write_eaf_file(df, "Traditional", "eaf_demand")
+    write_eaf_file(df, "Traditional", "steel_cogen")
+    write_eaf_file(df, "Transformation", "eaf_demand")
+    write_eaf_file(df, "Transformation", "steel_cogen")
+
+
+if __name__ == "__main__":
+    main()
