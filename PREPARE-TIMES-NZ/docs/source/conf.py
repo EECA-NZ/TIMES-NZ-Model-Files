eba60d9bab2a52f41a3006669d3c316287ecc33d
Luke Searle <luke.searle@eeca.govt.nz>
2025-12-09 13:29:37 +1300

    sphinx build

---
+++ PREPARE-TIMES-NZ/docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# pylint:disable = all
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PREPARE-TIMES-NZ"
+copyright = "2025, Luke Searle, Achini Weerasinghe"
+author = "Luke Searle, Achini Weerasinghe"
+release = "v3.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ PREPARE-TIMES-NZ/docs/source/index.rst
@@ -0,0 +1,17 @@
+.. PREPARE-TIMES-NZ documentation master file, created by
+   sphinx-quickstart on Tue Dec  9 12:48:30 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+PREPARE-TIMES-NZ documentation
+==============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
