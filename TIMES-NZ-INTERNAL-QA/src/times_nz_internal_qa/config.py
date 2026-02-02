e279cefac616c2b7550b417cb0c2042909f669cc
Luke Searle <luke.searle@eeca.govt.nz>
2026-02-03 09:50:04 +1300

    minor update and testing gas price changes

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
@@ -3,7 +3,6 @@ Surfaces some config options for preprocessing and the app
 """
 
 current_scenarios = [
-    "traditional-v3_0_0",
-    #"traditional-v3_0_1-NOWEM",
-    "transformation-v3_0_0",
+    "traditional-v3_0_1",
+    "transformation-v3_0_1",
 ]
