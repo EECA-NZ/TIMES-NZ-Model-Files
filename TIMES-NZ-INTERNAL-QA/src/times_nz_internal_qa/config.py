8df3315e2dddb353c2c34c469a404851c3fd0829
Luke Searle <luke.searle@eeca.govt.nz>
2026-02-04 16:40:17 +1300

    incremnt to v302

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
@@ -3,7 +3,6 @@ Surfaces some config options for preprocessing and the app
 """
 
 current_scenarios = [
-    "traditional-v3_0_1",
-    "traditional-v3_0_1-gasprodprice",
-    "transformation-v3_0_1",
+    "traditional-v3_0_2",
+    "transformation-v3_0_2",
 ]
