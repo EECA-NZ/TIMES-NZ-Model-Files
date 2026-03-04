48b054c8fb9412ef6d7ebd956dc58eff2c27cb33
searleluke <68429780+searleluke@users.noreply.github.com>
2026-03-05 11:15:32 +1300

    Res space heating demand curve flex fix (#221)
    Force residential heating techs to match the COM_FR curves in their use, which stops the flexibility getting too high when meeting peak demand in high-cost slices.
    
    Also set all res processes to DAYNITE, mostly for reporting purposes.

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
@@ -5,8 +5,7 @@ Surfaces some config options for preprocessing and the app
 current_scenarios = [
     "traditional-v3_0_3",
     "transformation-v3_0_3",
-    # "traditional-v3_0_2-cheapgrid",
-    # "transformation-v3_0_2-cheapgrid",
-    # "traditional-v3_0_2-cheapgenstack",
-    # "transformation-v3_0_2-cheapgenstack",
+    # "traditional-v3_0_3-resaf08",
+    # "traditional-v3_0_3-resafa08",
+    # "traditional-v3_0_3-resafslice08",
 ]
