aa6491b211658746d0d1f68aceb955eb4fb35c78
Luke Searle <luke.searle@eeca.govt.nz>
2026-03-19 14:43:31 +1300

    fix petrol emissions in residential, gitignore some local analysis

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
@@ -4,7 +4,7 @@ Surfaces some config options for preprocessing and the app
 
 current_scenarios = [
     "traditional-v304",
-    "traditional-v304-freehthp",
-    "traditional-v304-freelng",
+    # "traditional-v304-freehthp",
+    # "traditional-v304-freelng",
     "transformation-v304",
 ]
