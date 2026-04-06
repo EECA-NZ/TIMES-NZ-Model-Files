acffbc3c8e97446a13d43738c2ff49e17fddc6d0
Luke Searle <luke.searle@eeca.govt.nz>
2026-04-07 11:40:45 +1200

    DF outputs and fix dodo

--- PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_load_curves.py
+++ PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_load_curves.py
@@ -3,14 +3,17 @@ Orchestrates building of Veda files for demand projections
 """
 
 from prepare_times_nz.stage_4.load_curves import main as load_curves
+from prepare_times_nz.stage_4.technology_af_curves import main as tech_af_curves
 
 
 def main():
     """
     Creates veda files for load curves
     (also known as the COM_FR scenarios)
+    As well as the technology AFs for some technologies
     """
     load_curves()
+    tech_af_curves()
 
 
 if __name__ == "__main__":
