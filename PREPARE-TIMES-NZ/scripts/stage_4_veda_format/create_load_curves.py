43e6449d64277d7fc91e2b237cf5ad4ce554fb2f
Luke Searle <luke.searle@eeca.govt.nz>
2026-04-07 14:32:51 +1200

    remove orphaned reference

--- PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_load_curves.py
+++ PREPARE-TIMES-NZ/scripts/stage_4_veda_format/create_load_curves.py
@@ -3,7 +3,6 @@ Orchestrates building of Veda files for demand projections
 """
 
 from prepare_times_nz.stage_4.load_curves import main as load_curves
-from prepare_times_nz.stage_4.technology_af_curves import main as tech_af_curves
 
 
 def main():
@@ -13,7 +12,6 @@ def main():
     As well as the technology AFs for some technologies
     """
     load_curves()
-    tech_af_curves()
 
 
 if __name__ == "__main__":
