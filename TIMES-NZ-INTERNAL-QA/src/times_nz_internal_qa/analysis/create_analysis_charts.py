0d8ba0ba9e75496bf6825543c048d7346b3d217e
Luke Searle <luke.searle@eeca.govt.nz>
2026-07-01 10:31:52 +1200

    refactor analysis main

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/analysis/create_analysis_charts.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/analysis/create_analysis_charts.py
@@ -15,21 +15,16 @@ from times_nz_internal_qa.analysis import (
     analysis_transport,
 )
 
-ANALYSIS_CHART_MODULES = [
-    analysis_indicators,
-    analysis_electricity_generation,
-    analysis_emissions,
-    analysis_primary_energy,
-    analysis_demand,
-    analysis_transport,
-]
-
 
 def main():
     """Run every subject-area chart module."""
 
-    for chart_module in ANALYSIS_CHART_MODULES:
-        chart_module.main()
+    analysis_indicators.main()
+    analysis_electricity_generation.main()
+    analysis_emissions.main()
+    analysis_primary_energy.main()
+    analysis_demand.main()
+    analysis_transport.main()
 
 
 if __name__ == "__main__":
