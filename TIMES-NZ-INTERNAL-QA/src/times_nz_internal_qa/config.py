a3eb784513d88d69b7575ff5636b8bbea6b7e900
Luke Searle <luke.searle@eeca.govt.nz>
2026-07-28 14:28:26 +1200

    setup for deployment

--- TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
+++ TIMES-NZ-INTERNAL-QA/src/times_nz_internal_qa/config.py
@@ -1,7 +1,7 @@
 """
 Surfaces some config options for preprocessing and the app
-These are the scenarios that will populate the cleaned parquet files 
-in post processing 
+These are the scenarios that will populate the cleaned parquet files
+in post processing
 These parquet files serve the app but can also be used for detailed analysis.
 For sensitivty testing, we might want to expand the batch to variant scenarios
 However, it's best not to upload many variant scenarios if they won't populate the app
@@ -12,19 +12,16 @@ So it might be desirable to trim the list for app uploads rather than
 including dozens of variants. The data size is non-trivial.
 """
 
-# MAIN BATCH 
+# MAIN BATCH
 
 
 current_scenarios = [
     "steady-v308",
+    "shift-v308",
     "steady-v308-noflex",
     "steady-v308-nodf",
-    "steady-v308-shiftdf",
     "steady-v308-nobatt",
-    "shift-v308",
     "shift-v308-noflex",
     "shift-v308-nodf",
-    "shift-v308-steadydf",
     "shift-v308-nobatt",
 ]
-
